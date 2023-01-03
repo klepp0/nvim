aef779bb1ddcc790a7ad9d09404706b0caf67905
Jonas Kleppich <kleppo@Jonass-MacBook-Pro.local>
2023-01-03 20:44:13 +0100

    feat: add shorcuts for chmod +x/copy/paste

--- lua/sacklippe/remap.lua
+++ lua/sacklippe/remap.lua
@@ -6,3 +6,12 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- center cursor 
 vim.keymap.set({"n", "v"}, "<C-d>", "<C-d>zz")
 vim.keymap.set({"n", "v"}, "<C-u>", "<C-u>zz")
+
+-- chmod +x file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- copy/paste to clipboard
+vim.api.nvim_set_keymap("", "<leader>y", '"+y', { silent = true })
+vim.api.nvim_set_keymap("", "<leader>Y", '"+Y', { silent = true })
+vim.api.nvim_set_keymap("", "<leader>p", '"+p', { silent = true })
+vim.api.nvim_set_keymap("", "<leader>P", '"+P', { silent = true })
