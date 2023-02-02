3f3087876fa977407f3ee71b2e3e727f1718df9a
Jonas Kleppich <kleppo@Jonass-MBP.fritz.box>
2023-02-02 19:15:59 +0100

    feat: first lsp configuration with lsp zero

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,33 @@
+-- Learn the keybindings, see :help lsp-zero-keybindings
+-- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<C-n>", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "<C-p>", function()
+		vim.diagnostic.goto_prew()
+	end, opts)
+end)
+
+lsp.setup()
