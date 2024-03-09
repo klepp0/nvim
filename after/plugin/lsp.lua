f02a57a89960f79f5c21562540e227d66139ac15
sacklippe <jonas.kleppich@voltfang.de>
2024-03-09 19:37:08 +0100

    feat: add markdown formatting with prettier

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -71,5 +71,8 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = { "markdown" },
+        }),
     }
 })
