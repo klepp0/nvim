aa1862b436850c1fa0c0b7da65750a052a20a38c
sacklippe <jonas.kleppich@voltfang.de>
2024-06-02 22:53:40 +0200

    feat: add remap for horizontal and vertical window split

--- lua/klepp0/remap.lua
+++ lua/klepp0/remap.lua
@@ -1,1 +1,13 @@
-
+-- split window
+vim.keymap.set(
+	"n",
+	"<C-x>",
+	"<cmd>silent :vsp<CR> :wincmd l<CR>",
+	{ desc = "split window at the [x]-axis (horizontally)" }
+)
+vim.keymap.set(
+	"n",
+	"<C-y>",
+	"<cmd>silent :sp<CR> :wincmd j<CR>",
+	{ desc = "split window at the [y]-axis (vertically)" }
+)
