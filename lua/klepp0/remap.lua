c09c407bb32a8f14f02a52e1bd38716152aeb468
sacklippe <jonas.kleppich@voltfang.de>
2024-06-03 18:10:29 +0200

    fix: swap horizontal and vertical window split

--- lua/klepp0/remap.lua
+++ lua/klepp0/remap.lua
@@ -2,13 +2,13 @@
 vim.keymap.set(
 	"n",
 	"<C-x>",
-	"<cmd>silent :vsp<CR> :wincmd l<CR>",
+	"<cmd>silent :sp<CR> :wincmd j<CR>",
 	{ desc = "Split window at the [x]-axis (horizontally)" }
 )
 vim.keymap.set(
 	"n",
 	"<C-y>",
-	"<cmd>silent :sp<CR> :wincmd j<CR>",
+	"<cmd>silent :vsp<CR> :wincmd l<CR>",
 	{ desc = "Split window at the [y]-axis (vertically)" }
 )
 
