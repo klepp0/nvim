9b3cd8718eba8687c8468da7cf5debf95894aa12
sacklippe <jonas.kleppich@voltfang.de>
2024-06-03 17:42:53 +0200

    refactor: remove yank autocommand to set file

--- lua/klepp0/set.lua
+++ lua/klepp0/set.lua
@@ -66,9 +66,21 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- Enable 24-bit RGB color in the TUI
 vim.opt.termguicolors = true
 
+-- Set vertical bar at the 100th charactor as orientation for the line length
 vim.opt.colorcolumn = "100"
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
