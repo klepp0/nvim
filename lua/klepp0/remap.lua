0507ce6e45ec227011b67c4e5503f3fc5f4515d9
sacklippe <jonas.kleppich@voltfang.de>
2024-06-03 17:43:31 +0200

    refactor: migrate and configure remaps to remap file

--- lua/klepp0/remap.lua
+++ lua/klepp0/remap.lua
@@ -3,11 +3,37 @@ vim.keymap.set(
 	"n",
 	"<C-x>",
 	"<cmd>silent :vsp<CR> :wincmd l<CR>",
-	{ desc = "split window at the [x]-axis (horizontally)" }
+	{ desc = "Split window at the [x]-axis (horizontally)" }
 )
 vim.keymap.set(
 	"n",
 	"<C-y>",
 	"<cmd>silent :sp<CR> :wincmd j<CR>",
-	{ desc = "split window at the [y]-axis (vertically)" }
+	{ desc = "Split window at the [y]-axis (vertically)" }
 )
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<C-n>", vim.diagnostic.goto_next, { desc = "Go to [n]ext diagnostic message" })
+vim.keymap.set("n", "<C-N>", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Enter/exit terminal mode in the builtin terminal
+vim.keymap.set("n", "<C-t>", "<cmd>term<CR>", { desc = "Enter terminal mode" })
+vim.keymap.set("t", "<C-t>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds for split navigation
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
