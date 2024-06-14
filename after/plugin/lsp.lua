f9177003de0e9c59f2c0e14cacc287b2104be4c5
sacklippe <jonas.kleppich@voltfang.de>
2024-06-14 18:23:11 +0200

    refactor: migrate cmp plugin into seperate file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -115,28 +115,3 @@ conform.setup({
 	},
 })
 
--- Autocomplete
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-
-luasnip.config.setup({})
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	mapping = {
-		--		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		--		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		--		["<C-Space>"] = cmp.mapping.complete(),
-		--		["<C-e>"] = cmp.mapping.close(),
-		--		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	},
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "path" },
-	},
-})
