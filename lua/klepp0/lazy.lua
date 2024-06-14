e231acc842cc946d78d8d717755077de440143c2
sacklippe <jonas.kleppich@voltfang.de>
2024-06-14 17:42:12 +0200

    refactor: gitsign config had been setup twice

--- lua/klepp0/lazy.lua
+++ lua/klepp0/lazy.lua
@@ -41,15 +41,15 @@ require("lazy").setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    },
+    --    opts = {
+    --      signs = {
+    --        add = { text = "+" },
+    --        change = { text = "~" },
+    --        delete = { text = "_" },
+    --        topdelete = { text = "‾" },
+    --        changedelete = { text = "~" },
+    --      },
+    --    },
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
