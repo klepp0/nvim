b1d7fd8c780bcff8e5f1ac5e70238058e1a18e22
klepp0 <jonas.kleppich@sap.com>
2025-04-13 18:31:57 +0200

    refactor: improve copy/paste from clipboard

--- lua/klepp0/remap.lua
+++ lua/klepp0/remap.lua
@@ -55,19 +55,11 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Copy/paste to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
-vim.keymap.set({ "n", "v" }, "<leader>d", '"+d')
-vim.keymap.set({ "n" }, "<leader>Y", '"+Y')
-vim.keymap.set({ "n" }, "<leader>D", '"+D')
-vim.keymap.set("n", "<leader>p", function()
-	vim.opt.autoindent = false
-	vim.opt.smartindent = false
-	vim.opt.cindent = false
-	vim.cmd('normal! "+p')
-	vim.opt.autoindent = true
-	vim.opt.smartindent = true
-	vim.opt.cindent = true
-end, { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>d", '"+d', { desc = "Delete and copy to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n" }, "<leader>Y", '"+Y', { desc = "Copy line to system clipboard" })
+vim.keymap.set({ "n" }, "<leader>D", '"+D', { desc = "Delete line and copy to system clipboard" })
 
 
 -- Format code
